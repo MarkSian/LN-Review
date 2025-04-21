f3f33e6bf90ffe83d15ab8b76c21bb078152713b
MarkSian <merksianipar@gmail.com>
2025-04-21 06:15:40 -0600

    middleware error handling

--- server.mjs
+++ server.mjs
@@ -40,3 +40,9 @@ let users = [
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+// Middleware For Error Handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
