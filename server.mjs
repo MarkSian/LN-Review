94c48e5dbba490d01d116692ef3db11ab8e7a7ac
MarkSian <merksianipar@gmail.com>
2025-04-21 06:09:55 -0600

    Data and logging middleware

--- server.mjs
+++ server.mjs
@@ -7,6 +7,33 @@ app.get('/', (req, res) => {
     res.send('LN-Review');
 })
 
+// Custom middleware to log request details
+app.use((req, res, next) => {
+    console.log(`${req.method} request for ${req.url}`);
+    next();
+});
+
+
+// Data storage for the project
+let novels = [
+    { id: 1, title: '"JoJos Bizarre Adventure', author: 'Hirohiko Araki', genre: 'Action' },
+    { id: 2, title: 'Overlord', author: 'Kugane Maruyama', genre: 'Dark Fantasy' },
+    { id: 3, title: 'Re:Zero', author: 'Tappei Nagatsuki', genre: 'Fantasy' }
+];
+
+let reviews = [
+    { id: 1, novelId: 1, userId: 1, rating: 5, comment: 'Amazing character development and world building!' },
+    { id: 2, novelId: 1, userId: 2, rating: 4, comment: 'Great world-building and love the dark setting' },
+    { id: 3, novelId: 2, userId: 1, rating: 5, comment: 'Re:zero is a beautifully made light novel and the anime was adapted well.' }
+];
+
+let users = [
+    { id: 1, username: 'lightnovel_fan', joinDate: '2024-01-19' },
+    { id: 2, username: 'fantasy_person', joinDate: '2024-01-20' }
+];
+
+
+//app.listen to start the server *keep at the bottom of the file*
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
